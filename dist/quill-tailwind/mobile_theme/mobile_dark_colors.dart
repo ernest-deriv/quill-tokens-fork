c98056fcc0b562812541dffff44b52aae0e43b41
ernest-deriv <120568427+ernest-deriv@users.noreply.github.com>
2023-12-15 18:17:26 +0400

    Workflow fourteenth draft

---
+++ dist/quill-tailwind/mobile_theme/mobile_dark_colors.dart
@@ -0,0 +1,39 @@
+
+//
+// mobile_theme/mobile_dark_colors.dart
+//
+
+// Do not edit directly
+// Generated on Fri, 15 Dec 2023 14:17:05 GMT
+
+
+
+import 'dart:ui';
+
+class MobileDarkColors {
+  MobileDarkColors._();
+
+    static const semanticColorBackgroundDialog = #181c25;
+    static const semanticColorBackgroundOverflow1Bottom = linear-gradient(181.93deg, rgba( #11141b ,0%) 1.63%, #11141b 50.01%);
+    static const semanticColorBackgroundOverflow1Left = linear-gradient(271.93deg, rgba( #11141b ,0%) 1.63%, #11141b 49.98%);
+    static const semanticColorBackgroundOverflow1Right = linear-gradient(88.07deg, rgba( #11141b ,0%) 1.63%, #11141b 50.02%);
+    static const semanticColorBackgroundOverflow1Top = linear-gradient(1.93deg, rgba( #11141b ,0%) 1.56%, #11141b 49.91%);
+    static const semanticColorBackgroundOverflow2Bottom = linear-gradient(181.93deg, rgba( #181c25 ,0%) 1.63%, #181c25 50.01%);
+    static const semanticColorBackgroundOverflow2Left = linear-gradient(271.93deg, rgba( #181c25 ,0%) 1.63%, #181c25 49.98%);
+    static const semanticColorBackgroundOverflow2Right = linear-gradient(88.07deg, rgba( #181c25 ,0%) 1.63%, #181c25 50.02%);
+    static const semanticColorBackgroundOverflow2Top = linear-gradient(1.93deg, rgba( #181c25 ,0%) 1.56%, #181c25 49.91%);
+    static const semanticColorBackgroundOverlay = rgba(#000000,72%);
+    static const semanticColorBackgroundPrimaryBase = #11141b;
+    static const semanticColorBackgroundPrimaryContainer = #181c25;
+    static const semanticColorBackgroundSecondaryBase = #11141b;
+    static const semanticColorBackgroundSecondaryContainer = #181c25;
+    static const semanticColorStatusDanger = #f92e26;
+    static const semanticColorStatusInfo = #3daaff;
+    static const semanticColorStatusSuccess = #26a44e;
+    static const semanticColorStatusWarning = #ffae26;
+    static const semanticColorTypographyDefault = rgba(#ffffff,72%);
+    static const semanticColorTypographyDisabled = rgba(#ffffff,24%);
+    static const semanticColorTypographyLink = #ff444f;
+    static const semanticColorTypographyProminent = #ffffff;
+    static const semanticColorTypographySubtle = rgba(#ffffff,48%);
+}
---
+++ dist/quill-tailwind/mobile_theme/mobile_light_colors.dart
@@ -0,0 +1,39 @@
+
+//
+// mobile_theme/mobile_light_colors.dart
+//
+
+// Do not edit directly
+// Generated on Fri, 15 Dec 2023 14:17:06 GMT
+
+
+
+import 'dart:ui';
+
+class MobileLightColors {
+  MobileLightColors._();
+
+    static const semanticColorBackgroundDialog = #ffffff;
+    static const semanticColorBackgroundOverflow1Bottom = linear-gradient(181.93deg, rgba( #ffffff,0%) 1.63%, #ffffff 50.01%);
+    static const semanticColorBackgroundOverflow1Left = linear-gradient(271.93deg, rgba( #ffffff,0%) 1.63%, #ffffff 49.98%);
+    static const semanticColorBackgroundOverflow1Right = linear-gradient(88.07deg, rgba( #ffffff,0%) 1.63%, #ffffff 50.02%);
+    static const semanticColorBackgroundOverflow1Top = linear-gradient(1.93deg, rgba( #ffffff,0%) 1.56%, #ffffff 49.91%);
+    static const semanticColorBackgroundOverflow2Bottom = linear-gradient(181.93deg, rgba( #f6f7f8,0%) 1.63%, #f6f7f8 50.01%);
+    static const semanticColorBackgroundOverflow2Left = linear-gradient(271.93deg, rgba( #f6f7f8,0%) 1.63%, #f6f7f8 49.98%);
+    static const semanticColorBackgroundOverflow2Right = linear-gradient(88.07deg, rgba( #f6f7f8,0%) 1.63%, #f6f7f8 50.02%);
+    static const semanticColorBackgroundOverflow2Top = linear-gradient(1.93deg, rgba( #f6f7f8,0%) 1.56%, #f6f7f8 49.91%);
+    static const semanticColorBackgroundOverlay = rgba(#000000,72%);
+    static const semanticColorBackgroundPrimaryBase = #f6f7f8;
+    static const semanticColorBackgroundPrimaryContainer = #ffffff;
+    static const semanticColorBackgroundSecondaryBase = #ffffff;
+    static const semanticColorBackgroundSecondaryContainer = #f6f7f8;
+    static const semanticColorStatusDanger = #db0800;
+    static const semanticColorStatusInfo = #1789e1;
+    static const semanticColorStatusSuccess = #00822a;
+    static const semanticColorStatusWarning = #e18d00;
+    static const semanticColorTypographyDefault = rgba(#000000,72%);
+    static const semanticColorTypographyDisabled = rgba(#000000,24%);
+    static const semanticColorTypographyLink = #ff444f;
+    static const semanticColorTypographyProminent = #000000;
+    static const semanticColorTypographySubtle = rgba(#000000,48%);
+}
